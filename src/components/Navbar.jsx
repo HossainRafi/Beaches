af3589802ce75c4c715742b8e487cd76dab1632c
HossainRafi <hossainrafi71@gmail.com>
2022-11-29 12:06:20 +0600

    hambarger added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsPerson } from "react-icons/bs";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+
 
 const Navbar = () => {
-  
+ 
 
   return (
     <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white bg-slate-500">
@@ -17,6 +22,19 @@ const Navbar = () => {
         <li>View</li>
         <li>Book</li>
       </ul>
+      <div className="hidden md:flex">
+        <BiSearch className="" size={20} />
+        <BsPerson size={20} />
+      </div>
+
+      {/* Hamburger */}
+      <div onClick={handleNav} className="md:hidden z-10">
+        {nav ? (
+          <AiOutlineClose className="text-black" size={20} />
+        ) : (
+          <HiOutlineMenuAlt4 size={20} />
+        )}
+      </div>
     </div>
   );
 };
