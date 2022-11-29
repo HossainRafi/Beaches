55394fc2df0e8a0d1f34c05d90a5731520322edd
HossainRafi <hossainrafi71@gmail.com>
2022-11-29 14:39:40 +0600

    select card done

--- src/components/Selects.jsx
+++ src/components/Selects.jsx
@@ -6,17 +6,17 @@ import Maldives from "../assets/maldives.jpg";
 import Maldives2 from "../assets/maldives2.jpg";
 import Maldives3 from "../assets/maldives3.jpg";
 import KeyWest from "../assets/keywest.jpg";
-import SelectsCard from "./SelectsCard";
+import SelectCard from "./SelectCard";
 
 const Selects = () => {
   return (
     <div className="max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <SelectsCard bg={BoraBora} text="Bora Bora" />
-      <SelectsCard bg={BoraBora2} text="Maldives" />
-      <SelectsCard bg={Maldives} text="Antigua" />
-      <SelectsCard bg={Maldives2} text="Cozumel" />
-      <SelectsCard bg={Maldives3} text="Jamaica" />
-      <SelectsCard bg={KeyWest} text="Key West" />
+      <SelectCard bg={BoraBora} text="Bora Bora" />
+      <SelectCard bg={BoraBora2} text="Maldives" />
+      <SelectCard bg={Maldives} text="Antigua" />
+      <SelectCard bg={Maldives2} text="Cozumel" />
+      <SelectCard bg={Maldives3} text="Jamaica" />
+      <SelectCard bg={KeyWest} text="Key West" />
     </div>
   );
 };
