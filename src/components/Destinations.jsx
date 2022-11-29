051b4a5df4f6b1724823f85b1e1ff25535def5e1
HossainRafi <hossainrafi71@gmail.com>
2022-11-29 13:30:40 +0600

    heading and text

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+
+
 
 const Destinations = () => {
   return (
-    <div>Destinations</div>
-  )
-}
+    <div className="max-w-[1240px] mx-auto py-16 px-4 text-center">
+      <h1>All-Inclusive Resorts</h1>
+      <p className="py-4">On the Caribbean's Best Beaches</p>
+      
+    </div>
+  );
+};
 
-export default Destinations
+export default Destinations;
